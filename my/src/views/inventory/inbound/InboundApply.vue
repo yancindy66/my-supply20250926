611e62fa7c4c78007fed91476416edd168ca5082
yancindy66 <yancindy66@users.noreply.github.com>
2025-10-05 09:57:48 +0800

    feat(inventory): 入库申请复用车辆入库Luckysheet页面到 InboundApply.vue

--- my/src/views/inventory/inbound/InboundApply.vue
+++ my/src/views/inventory/inbound/InboundApply.vue
@@ -1,9 +1,10 @@
 <template>
-  <InboundReservation />
+  <VehicleInbound />
 </template>
 
 <script setup lang="ts">
-import InboundReservation from '@/views/inventory/inbound/InboundReservation.vue';
+// 复用“车辆入库（Luckysheet）”完整页面内容
+import VehicleInbound from '@/views/placeholder/InboundOrderOfficeFixed.vue';
 </script>
 
 
